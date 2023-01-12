3ea24e642bdc1f144e57a0e53e75b6f772e035a9
bennyhodl <ben@blockspaces.com>
2023-01-11 20:45:19 -0500

    Prep for LDK

--- src/ldk/index.ts
+++ src/ldk/index.ts
@@ -1,44 +1,26 @@
-import { TAccount, THeader } from "@synonymdev/react-native-ldk"
-import { getItem, setItem } from "../storage"
-
-export const setWallet = async ({name, seed}: TAccount) => {
-  const account: TAccount = {
-    name: name,
-    seed: seed
-  }
-  console.log(account)
-  // await Keychain.setGenericPassword(name, JSON.stringify(account), {service: name})
-  await setItem(account.name, account.seed)
-
-}
-export const createNewWallet = async () => {
-  try {
-
-    const accountName = "bay-wallet-0"
-    const account: TAccount = {
-      name: accountName,
-      seed: generateSeed()
-    }
-    
-    await setWallet(account)
-  } catch (e) {
-    console.error("create wallet", e)
-  }
-}
-
-const generateSeed = () => {
-  return "C43D4437B3CDF8FB1F6509AF7B6FEC10"
-}
-
-/**
- * Saves new/latest header data to local storage.
- * @param {THeader} header
- * @returns {Promise<void>}
- */
-export const updateHeader = async ({
-	header,
-}: {
-	header: THeader;
-}): Promise<boolean> => {
-	return await setItem('header', JSON.stringify(header));
-}; 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import RNFS from 'react-native-fs';
+// import {
+// 	getBlockHashFromHeight,
+// 	getBlockHeader,
+// 	getBlockHex,
+// 	getScriptPubKeyHistory,
+// } from '../electrum';
+import lm, {
+	DefaultTransactionDataShape,
+	TAccount,
+	TAccountBackup,
+	THeader,
+	TTransactionData,
+	TTransactionPosition,
+} from '@synonymdev/react-native-ldk';
+import ldk from '@synonymdev/react-native-ldk/dist/ldk';
+// import {
+// 	getAccount,
+// 	getAddress,
+// 	getNetwork,
+// 	ldkNetwork,
+// 	setAccount,
+// } from '../utils/helpers';
+// import { EAccount } from '../utils/types';
+import * as bitcoin from 'bitcoinjs-lib';

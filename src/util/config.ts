56660bb20ebbe9b4c9ec2123b8340bf92fda885c
bennyhodl <ben@blockspaces.com>
2023-01-14 22:24:11 -0500

    no need for api url

--- src/util/config.ts
+++ src/util/config.ts
@@ -2,6 +2,16 @@ import { TAvailableNetworks } from "@synonymdev/react-native-ldk";
 
 export const selectedNetwork: TAvailableNetworks = 'bitcoinRegtest'
 
-export const getApiUrl = (path:string): string => {
-  return `http://localhost:30000${path}`
-}
+//Electrum Server Info (Synonym Regtest Set By Default)
+export const customPeers = {
+	bitcoin: [],
+	bitcoinTestnet: [],
+	bitcoinRegtest: [
+		{
+			host: '35.233.47.252',
+			ssl: 18484,
+			tcp: 18483,
+			protocol: 'tcp',
+		},
+	],
+};

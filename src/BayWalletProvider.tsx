73d74aedb91d571da17e7cc6b6d49080c74e3b09
bennyhodl <ben@styng.social>
2023-01-23 22:28:08 -0500

    Icons

--- src/BayWalletProvider.tsx
+++ src/BayWalletProvider.tsx
@@ -2,16 +2,20 @@ import React from "react"
 import { Provider } from "mobx-react"
 import stores from "./store"
 import { NavigationContainer } from "@react-navigation/native"
-import { BaseComponent } from "./components/base-component"
-import { ApplicationProvider } from "@ui-kitten/components"
+import { ApplicationProvider, IconRegistry } from "@ui-kitten/components"
 import * as eva from "@eva-design/eva"
+import { EvaIconsPack } from "@ui-kitten/eva-icons"
+import { BaseComponent } from "./components/base-component"
 
 export const BayWalletProvider = ({children}) => {
   return (
     <NavigationContainer>
       <Provider lightning={stores.lightningStore} account={stores.accountStore}>
+        <IconRegistry icons={EvaIconsPack} />
         <ApplicationProvider {...eva} theme={eva.dark}>
-          {children}
+          <BaseComponent>
+            {children}
+          </BaseComponent>
         </ApplicationProvider>
       </Provider>
     </NavigationContainer>
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,7 +3,7 @@ import Home from "../screens/Home"
 import { Receive } from "../screens/Receive"
 import { Send } from "../screens/Send"
 import { Settings } from "../screens/Settings"
-import { BottomNavigation, BottomNavigationTab } from "@ui-kitten/components"
+import { BottomNavigation, BottomNavigationTab, Icon } from "@ui-kitten/components"
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 
@@ -25,15 +25,15 @@ const BottomTabBar = ({ navigation, state }) => (
   <BottomNavigation
     selectedIndex={state.index}
     onSelect={index => navigation.navigate(state.routeNames[index])}>
-    <BottomNavigationTab title='Home'/>
-    <BottomNavigationTab title='Settings'/>
+    <BottomNavigationTab icon={<Icon name="home-outline"/>}/>
+    <BottomNavigationTab icon={<Icon name="settings-outline" />}/>
   </BottomNavigation>
 );
 
 export const BayWalletNavigator = () => (
   <Navigator tabBar={props => <BottomTabBar {...props} />}>
-    <Screen name='baywallet' component={BayWalletAppNavigator}/>
-    <Screen name='settings' component={Settings}/>
+    <Screen name='baywallet' options={{headerShown:false}} component={BayWalletAppNavigator}/>
+    <Screen name='settings' options={{headerShown:false}} component={Settings}/>
   </Navigator>
 );
 

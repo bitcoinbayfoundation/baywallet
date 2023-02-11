000f7fee0d848b5608d7593829660fec07cd0669
bennyhodl <ben@styng.social>
2023-02-10 20:45:55 -0500

    Maybe foing with this color?

--- theme.ts
+++ theme.ts
@@ -1,18 +1,23 @@
-const ELECTRIC_BLUE = "#7DF9FF"
-const ELECTRIC_BLUE_DARK_BG = "#001A1B"
-const WHITE = "#FFFFFF"
+const AZURE_BLUE = "#3393FF"
+const AZURE_BLUE_DARK_BG = "#000431"
+const CESTASIAN_BLUE = "#01065A"
+const PANTONE_BLUE = "#021CA4"
+const BRANDEIS_BLUE = "#0275F4"
+const WHITE = "#F5FAFF"
 
 export const colors = {
+  "white": WHITE,
   "color-basic-100": WHITE, // Font color
-  "color-basic-200": ELECTRIC_BLUE,
-  "color-basic-300": ELECTRIC_BLUE,
-  "color-basic-400": ELECTRIC_BLUE,
-  "color-basic-500": ELECTRIC_BLUE,
-  "color-basic-600": ELECTRIC_BLUE,
-  "color-basic-700": ELECTRIC_BLUE,
-  "color-basic-800": ELECTRIC_BLUE_DARK_BG, // Dark theme background
-  "color-basic-900": ELECTRIC_BLUE_DARK_BG,
-  "color-basic-1000": ELECTRIC_BLUE_DARK_BG,
-  "color-basic-1100": ELECTRIC_BLUE_DARK_BG,
-  "background-basic-color-1": ELECTRIC_BLUE_DARK_BG // Dark theme background
+  "color-basic-200": AZURE_BLUE,
+  "color-basic-300": AZURE_BLUE,
+  "color-basic-400": AZURE_BLUE,
+  "color-basic-500": AZURE_BLUE,
+  "color-basic-600": PANTONE_BLUE,
+  "color-basic-700": CESTASIAN_BLUE,
+  "color-basic-800": AZURE_BLUE_DARK_BG, // Dark theme background
+  "color-basic-900": AZURE_BLUE_DARK_BG,
+  "color-basic-1000": AZURE_BLUE_DARK_BG,
+  "color-basic-1100": AZURE_BLUE_DARK_BG,
+  "color-primary-default-border": AZURE_BLUE,
+  "background-basic-color-1": AZURE_BLUE_DARK_BG // Dark theme background
 }

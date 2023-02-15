001a7d83514bc2b639108803fe889b74316c1b0c
bennyhodl <ben@styng.social>
2023-02-14 21:48:48 -0500

    status bar

--- src/BayWalletProvider.tsx
+++ src/BayWalletProvider.tsx
@@ -6,6 +6,7 @@ import { BaseComponent } from "./components/base-component"
 import { default as mapping } from "../mapping.json"
 import { colors } from "../theme"
 import { DataStoreProvider } from "./store/DataProvider"
+import { StatusBar } from "react-native"
 
 export const BayWalletProvider = ({children}) => {
   return (
@@ -15,6 +16,7 @@ export const BayWalletProvider = ({children}) => {
       <ApplicationProvider {...eva} theme={{...eva.dark, ...colors}} customMapping={mapping}>
         <BaseComponent>
           <DataStoreProvider>
+            <StatusBar barStyle="light-content" />
             {children}
           </DataStoreProvider>
         </BaseComponent>
--- src/screens/settings/Channels.tsx
+++ src/screens/settings/Channels.tsx
@@ -1,6 +1,6 @@
-import { Button, Divider, Icon, Layout, Text, TopNavigation, TopNavigationAction } from "@ui-kitten/components";
+import { Divider, Icon, Layout, Modal, Text, TopNavigation, TopNavigationAction } from "@ui-kitten/components";
 import { observer } from "mobx-react";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDataStore } from "../../store/DataProvider";
 import { BaseComponent } from "../../components/base-component";
 import { Channel } from "../../components/lightning/channel";
@@ -12,6 +12,7 @@ type ChannelsScreenProp = NativeStackNavigationProp<SettingsParamList, "channels
 
 export const Channels = observer(() => {
   const navigation = useNavigation<ChannelsScreenProp>()
+  const [openChannel, setOpenChannel] = useState<boolean>(false)
   const {lightningStore, lightningStore: {channels}} = useDataStore()
 
   useEffect(() => {
@@ -19,17 +20,28 @@ export const Channels = observer(() => {
   }, [])
 
   return (
+    <>
     <BaseComponent>
       <TopNavigation
         title='Channels'
         alignment='center'
         accessoryLeft={<TopNavigationAction onPress={() => navigation.goBack()} icon={<Icon name="arrow-ios-back-outline" />}/>}
-        accessoryRight={<TopNavigationAction icon={<Icon name="plus-outline" />}/>}
+        accessoryRight={<TopNavigationAction onPress={() => setOpenChannel(true)} icon={<Icon name="plus-outline" />}/>}
       />
       <Divider />
       <Layout>
         {channels?.map(chan => <Channel key={chan.channel_id} channel={chan} />)}  
       </Layout>
     </BaseComponent>
+    <OpenChannelModal show={openChannel} setShow={setOpenChannel}/>
+    </>
   )
 })
+
+const OpenChannelModal = ({show, setShow}: {show: boolean, setShow: any}) => {
+  return (
+    <Modal style={{width: "75%", height: "50%", borderWidth: 1, borderColor: "#AAA", borderRadius: 25, display: "flex", justifyContent: "center", alignItems: "center"}} visible={show} onBackdropPress={() => setShow(false)}>
+      <Text>ooo look at me, opening channels!</Text>
+    </Modal>
+  )
+}

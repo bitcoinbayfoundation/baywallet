c9d1efa6d34d20d77306ae29a57affa46e2cb649
bennyhodl <ben@styng.social>
2023-06-06 21:20:15 -0400

    Caching profile

--- src/components/note.tsx
+++ src/components/note.tsx
@@ -1,28 +1,24 @@
-import React, { useState } from "react"
+import React from "react"
 import { Avatar, Divider, Layout, Text } from "@ui-kitten/components"
 import { Event } from "nostr-tools"
-import { useProfile } from "../nostr/useProfile"
 import { Pressable } from "react-native"
-import { useNavigation } from "@react-navigation/native"
+import { useCachedProfile } from "../hooks/nostr"
+import { useDataStore } from "../store"
 
 type NoteProps = { note: Event, navigation?: any}
 export const Note = ({note, navigation}:NoteProps) => {
-  // const navigation = useNavigation()
-  const { content } = note
-
-  const {data: profile} = useProfile({
-    pubkey: note.pubkey
-  })
-
+  const {keyStore: {nostrKeys}} = useDataStore()
+  const {profile} = useCachedProfile(note.pubkey)
+  
   return (
     <>
     <Layout style={{marginVertical: 10, display: "flex", flexDirection: "row"}}>
-      <Pressable onPress={() => navigation.navigate("nostr-profile", {pubkey: "3f194d7cf5c59eca0145ed7804f0a67c0cc17b6ff6b4bd585821160dcf9d785b"})}>
+      <Pressable onPress={() => navigation.navigate("nostr-profile", {pubkey: nostrKeys.pubkey})}>
         <Avatar size="medium" source={{uri: profile?.picture}} />
       </Pressable>
       <Layout style={{display: "flex", flexDirection: 'column', width: "85%", marginLeft: 10}}>
         <Text style={{color: "#AAA"}}>@{profile?.name}</Text>
-        <Text style={{marginTop: 5}}>{content}</Text>
+        <Text style={{marginTop: 5}}>{note.content}</Text>
       </Layout>
     </Layout>
     <Divider />

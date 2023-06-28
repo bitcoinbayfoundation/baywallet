40e2fd5357b2a9043d25bca3620bd5c2b0159955
bennyhodl <ben@styng.social>
2023-06-28 11:22:36 -0400

    settings init

--- src/screens/settings/Settings.tsx
+++ src/screens/settings/Settings.tsx
@@ -2,31 +2,33 @@ import React from "react";
 import { observer } from "mobx-react";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { Button, Icon, ListItem, Toggle, TopNavigation } from "@ui-kitten/components";
+import { Button, Icon, Toggle, TopNavigation } from "@ui-kitten/components";
 import { BaseComponent } from "../../components";
 import { useDataStore } from "../../store";
 import { SettingsParamList } from "../../navigation";
+import { ListItem, Text, Switch, View } from "react-native-ui-lib"
+import { StyleSheet } from "react-native";
 
 type SettingsScreenProp = NativeStackNavigationProp<SettingsParamList, 'settings'>
 
 export const Settings = observer(() => {
   const navigation = useNavigation<SettingsScreenProp>()
-  const {settingsStore: {settings}, settingsStore} = useDataStore()
-  
+  const { settingsStore: { settings }, settingsStore } = useDataStore()
+
   return (
     <BaseComponent>
-      <TopNavigation
-        title='Settings'
-        alignment='center'
-      />
-      <ListItem
-        title="Hide Balance"
-        accessoryRight={ <Toggle checked={settings.hideBalance} onChange={async () => await settingsStore.setSetting("hideBalance", !settings.hideBalance)} />}
-      />
-      <ListItem
-        title="Display Shitty Fiat"
-        accessoryRight={ <Toggle checked={settings.usd} onChange={async () => await settingsStore.setSetting("usd", !settings.usd)} />}
-      />
+      <ListItem>
+        <View row centerV style={styles.listItem}>
+          <Text>Hide Balance</Text>
+          <Switch value={settings.hideBalance} onValueChange={async () => await settingsStore.setSetting("hideBalance", !settings.hideBalance)} />
+        </View>
+      </ListItem>
+      <ListItem>
+        <View row centerV style={styles.listItem}>
+          <Text>Display Shitty Fiat</Text>
+          <Switch value={settings.usd} onValueChange={async () => await settingsStore.setSetting("usd", !settings.usd)} />
+        </View>
+      </ListItem>
       <ListItem
         title="Advanced User (LDK)"
         accessoryRight={<Icon name="arrow-ios-forward-outline" fill="#FFF" />}
@@ -40,4 +42,12 @@ export const Settings = observer(() => {
       <Button onPress={async () => settingsStore.settingsInit()}>wipe</Button>
     </BaseComponent>
   )
+})
+
+const styles = StyleSheet.create({
+  listItem: {
+    justifyContent: "space-between",
+    width: "100%",
+    paddingHorizontal: 10
+  }
 })

0fa7c0f0e41120c5786c0e8407e646acd8232042
bennyhodl <ben@bitcoinbay.live>
2023-07-02 19:14:00 -0400

    unused storage import

--- src/util/storage.ts
+++ src/util/storage.ts
@@ -1,7 +1,4 @@
 import AsyncStorage from "@react-native-async-storage/async-storage"
-// import { MMKV } from 'react-native-mmkv'
-
-// export const storage = new MMKV()
 
 export const setItem = async (key:string, value:any): Promise<boolean> => {
   try {

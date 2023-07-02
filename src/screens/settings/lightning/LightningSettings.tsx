0dcf61404e38396dbbda2a21add18a2f36d5c84a
bennyhodl <ben@bitcoinbay.live>
2023-07-02 13:51:45 -0400

    lightning settings page

--- src/screens/settings/lightning/LightningSettings.tsx
+++ src/screens/settings/lightning/LightningSettings.tsx
@@ -3,12 +3,12 @@ import { observer } from "mobx-react";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import Toast from "react-native-toast-message";
-import { Layout, TopNavigation, Divider, Icon, TopNavigationAction, ListItem } from "@ui-kitten/components";
+import { StyleSheet } from "react-native";
+import { View, TouchableOpacity } from "react-native-ui-lib";
 import { useDataStore } from "../../../store";
-import { BaseComponent } from "../../../components";
+import { BaseComponent, MediumText } from "../../../components";
 import { SettingsParamList } from "../../../navigation";
 import { lspNodeDev } from "../../../util/config";
-import { Modals, useModal } from "../../../hooks";
 import { log } from "../../../util/logger";
 
 type LightningSettingProps = NativeStackNavigationProp<SettingsParamList, "lightning-settings">
@@ -16,55 +16,51 @@ type LightningSettingProps = NativeStackNavigationProp<SettingsParamList, "light
 export const LightningSettings = observer(() => {
   const navigation = useNavigation<LightningSettingProps>()
   const { lightningStore, lightningStore: { nodeId } } = useDataStore()
-  const { showModal } = useModal()
+
   return (
     <BaseComponent>
-      <TopNavigation
-        title='Advanced Settings (LDK)'
-        alignment='center'
-        accessoryLeft={() => <TopNavigationAction onPress={() => navigation.goBack()} icon={<Icon name="arrow-ios-back-outline" />} />}
-      />
-      <Divider />
-      <Layout>
-        <ListItem
-          title="Get node id"
-          onPress={() => showModal(Modals.NodeId, { nodeId: nodeId })}
-        />
-        <ListItem
-          title="Channels"
-          accessoryRight={<Icon name="arrow-ios-forward-outline" fill="#FFF" />}
-          onPress={() => navigation.navigate("channels")}
-        />
-        <ListItem
-          title="Add peer"
-          onPress={async () => {
-            const peer = await lightningStore.addPeer(lspNodeDev.address, lspNodeDev.port, lspNodeDev.pubKey)
-            return alert(peer)
-          }}
-        />
-        <ListItem
-          title="Get peers"
-          onPress={async () => {
-            const peers = await lightningStore.getPeers()
-            if (peers.length === 0) return alert("No peers")
-            return Toast.show({ type: "success", text1: "Peers", text2: JSON.stringify(peers) })
-          }}
-        />
-        <ListItem
-          title="Create Invoice"
-          onPress={async () => {
-            const invoice = await lightningStore.createInvoice(100, "who you callin pinhead?")
-            log.ldk(invoice.to_str)
-          }}
-        />
-        <ListItem
-          title="Get node balance"
-          onPress={async () => {
-            const balance = await lightningStore.getNodeBalance()
-            return alert(balance + " sats")
-          }}
-        />
-      </Layout>
+      <View>
+        <TouchableOpacity style={styles.lightningSettings} onPress={() => console.log(nodeId)}>
+          <MediumText content="Node Id" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.lightningSettings} onPress={() => navigation.navigate("channels")} row>
+          <MediumText content="Channels" />
+          <MediumText content=">" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.lightningSettings} onPress={async () => {
+          const peer = await lightningStore.addPeer(lspNodeDev.address, lspNodeDev.port, lspNodeDev.pubKey)
+          return Toast.show({ type: "success", text1: "Added peer!", text2: JSON.stringify(peer) })
+        }} row>
+          <MediumText content="Add Peer" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.lightningSettings} onPress={async () => {
+          const peers = await lightningStore.getPeers()
+          if (peers.length === 0) return alert("No peers")
+          return Toast.show({ type: "success", text1: "Peers", text2: JSON.stringify(peers) })
+        }} row>
+          <MediumText content="Get Peers" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.lightningSettings} onPress={async () => {
+          const invoice = await lightningStore.createInvoice(100, "who you callin pinhead?")
+          log.ldk(invoice.to_str)
+        }} row>
+          <MediumText content="Create Invoice" />
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.lightningSettings} onPress={async () => {
+          const balance = await lightningStore.getNodeBalance()
+          return Toast.show({ type: "success", text1: "Peers", text2: `${balance} sats` })
+        }} row>
+          <MediumText content="Node Balance" />
+        </TouchableOpacity>
+      </View>
     </BaseComponent>
   )
+})
+
+const styles = StyleSheet.create({
+  lightningSettings: {
+    justifyContent: "space-between",
+    width: "100%",
+    padding: 10,
+  }
 })

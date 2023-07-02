5c38e2e7b23103111e5e80497d5173f9e66212a8
bennyhodl <ben@bitcoinbay.live>
2023-07-02 14:12:16 -0400

    channels

--- src/screens/settings/lightning/Channels.tsx
+++ src/screens/settings/lightning/Channels.tsx
@@ -1,19 +1,21 @@
 import React, { useEffect } from "react";
 import { observer } from "mobx-react";
-import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { Divider, Icon, Layout, TopNavigation, TopNavigationAction } from "@ui-kitten/components";
+import { View } from "react-native-ui-lib";
 import { useDataStore } from "../../../store";
 import { BaseComponent, Channel } from "../../../components";
 import { SettingsParamList } from "../../../navigation";
-import { useModal, Modals } from "../../../hooks";
 
 type ChannelsScreenProp = NativeStackNavigationProp<SettingsParamList, "channels">
 
-export const Channels = observer(() => {
-  const navigation = useNavigation<ChannelsScreenProp>()
+/**
+ * Channels screen
+ * TODO: Add channel creation
+ * TODO: Add modal to view channel details
+ * TODO: Pull to refresh
+ */
+export const Channels = observer((_: ChannelsScreenProp) => {
   const { lightningStore, lightningStore: { channels } } = useDataStore()
-  const { showModal } = useModal()
 
   useEffect(() => {
     lightningStore.getChannels()
@@ -21,16 +23,9 @@ export const Channels = observer(() => {
 
   return (
     <BaseComponent>
-      <TopNavigation
-        title='Channels'
-        alignment='center'
-        accessoryLeft={() => <TopNavigationAction onPress={() => navigation.goBack()} icon={<Icon name="arrow-ios-back-outline" />} />}
-        accessoryRight={() => <TopNavigationAction onPress={() => showModal(Modals.ChannelDetailed)} icon={<Icon name="plus-outline" />} />}
-      />
-      <Divider />
-      <Layout>
+      <View>
         {channels?.map(chan => <Channel key={chan.channel_id} channel={chan} />)}
-      </Layout>
+      </View>
     </BaseComponent>
   )
 })

0a2765eeee393bc5b7073529572f11ceb57621cf
bennyhodl <ben@bitcoinbay.live>
2023-07-02 21:27:13 -0400

    theming

--- src/BayWalletProvider.tsx
+++ src/BayWalletProvider.tsx
@@ -1,8 +1,10 @@
+require('react-native-ui-lib/config').setConfig({ appScheme: 'dark' });
 import React from "react"
 import { BaseComponent } from "./components/"
 import { DataStoreProvider } from "./store/DataProvider"
 import { StatusBar } from "react-native"
 import Toast, { BaseToast } from "react-native-toast-message"
+import { Colors } from "react-native-ui-lib"
 
 export const toastConfig = {
   success: (props) => (
@@ -28,3 +30,29 @@ export const BayWalletProvider = ({ children }) => {
     </BaseComponent>
   )
 }
+
+/**
+ * Theming
+ */
+Colors.loadColors({
+  error: "#FF391A",
+  success: "#1AFF57",
+  primary: "#FF581A",
+  backgroundLight: "#FFFFFF",
+  backgroundDark: "#000000",
+})
+
+Colors.loadSchemes({
+  light: {
+    primary: Colors.primary,
+    screenBG: Colors.backgroundLight,
+    text: Colors.black,
+    button: Colors.primary
+  },
+  dark: {
+    primary: Colors.primary,
+    screenBG: Colors.backgroundDark,
+    text: Colors.white,
+    button: Colors.primary
+  }
+})

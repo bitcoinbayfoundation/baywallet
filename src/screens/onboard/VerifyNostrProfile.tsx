0a76d5c079062bca6224837e2977d600e443f8e2
bennyhodl <ben@bitcoinbay.live>
2023-07-07 23:35:15 -0400

    verify account

--- src/screens/onboard/VerifyNostrProfile.tsx
+++ src/screens/onboard/VerifyNostrProfile.tsx
@@ -1,29 +1,55 @@
-import React from "react";
-import { Text, View } from "react-native-ui-lib";
-import { BaseComponent, Button, LargeText } from "../../components";
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-import { OnboardParamList } from "../../navigation";
-import { useNavigation } from "@react-navigation/native";
-import { styles } from "./styles";
-import { VerifyProfile } from "../../components/nostr";
+import React, { useEffect, useState } from 'react';
+import { Text, View } from 'react-native-ui-lib';
+import { BaseComponent, Button, LargeText } from '../../components';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+import { OnboardParamList } from '../../navigation';
+import { RouteProp, useNavigation } from '@react-navigation/native';
+import { styles } from './styles';
+import { VerifyProfile } from '../../components/nostr';
+import { useNostrKeys } from '../../hooks';
+import { nip19 } from 'nostr-tools';
 
-type VerifyNostrProfileScreenProps = NativeStackNavigationProp<OnboardParamList, "verify-nostr-profile">
+type VerifyNostrProfileScreenProps = NativeStackNavigationProp<
+  OnboardParamList,
+  'verify-nostr-profile'
+>;
 
-export const VerifyNostrProfile = () => {
-  const navigation = useNavigation<VerifyNostrProfileScreenProps>()
+type VerifyNostrProfileProps = {
+  route?: RouteProp<OnboardParamList, 'verify-nostr-profile'>;
+};
+
+export const VerifyNostrProfile = (props: VerifyNostrProfileProps) => {
+  const navigation = useNavigation<VerifyNostrProfileScreenProps>();
+  const { privatekey } = props.route.params;
+  const privkey = privatekey.startsWith('nsec')
+    ? (nip19.decode(privatekey).data as string)
+    : privatekey;
+  const { nostrKeys, saveKeys } = useNostrKeys(privkey);
 
   return (
     <BaseComponent>
       <View height="100%" style={styles.onboard}>
         <View centerH>
           <LargeText content="Is this you?" styles={{ marginBottom: 20 }} />
-          <VerifyProfile pubkey="3f194d7cf5c59eca0145ed7804f0a67c0cc17b6ff6b4bd585821160dcf9d785b" />
+          <VerifyProfile pubkey={nostrKeys?.pubkey} />
         </View>
         <View height={100} style={styles.buttons}>
-          <Button label="Yes!" size="large" onPress={() => navigation.navigate("lightning-introduction")} />
-          <Button label="Nope" size="large" onPress={() => navigation.navigate("nostr-login")} inverted />
+          <Button
+            label="Yes!"
+            size="large"
+            onPress={() => {
+              saveKeys();
+              navigation.navigate('lightning-introduction');
+            }}
+          />
+          <Button
+            label="Nope"
+            size="large"
+            onPress={() => navigation.navigate('nostr-login')}
+            inverted
+          />
         </View>
       </View>
     </BaseComponent>
-  )
-}
+  );
+};

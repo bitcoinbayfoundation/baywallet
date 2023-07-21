738998dcafb6d2ee54302935089dc5e69d4489c1
bennyhodl <ben@bitcoinbay.live>
2023-07-21 11:00:36 -0400

    example test

---
+++ __mock__/nostr.ts
@@ -0,0 +1,27 @@
+export const mockProfile = {
+  mockRawProfile: {
+    id: '2173032c76182a08cfcc486c8dca28c09cad72dc39d6a02ff1aa938d89ae6000',
+    kind: 0,
+    pubkey: '43c32ed61a8259ddaef6eb43c1c3114524c09690cbb92c57196b395abf73c527',
+    tags: [],
+    created_at: 1688098111,
+    content:
+      '{"website":"https://github.com/bennyhodl/baywallet","lud06":"lnurl1dp68gurn8ghj7ampd3kx2ar0veekzar0wd5xjtnrdakj7tnhv4kxctttdehhwm30d3h82unvwqhkwmrpwdehjurjd9jhxapkxytx7p52","nip05":"baywallet@bitcoinbay.club","picture":"https://void.cat/d/JmvLC9iEZfkN8hxTJFbXRu.webp","display_name":"Bay Wallet","about":"tampa is the best city in the us","name":"baywallet","banner":"https://nostr.build/i/b56a69d047968e88f2d37392c09a9870506459f4b09857d049f6f92f84d420d6.jpg","lud16":"glassypriest16@walletofsatoshi.com"}',
+    sig: '081629f911a04e401787136bbea268897c9ba0c8def5986d7f7e4d1acd7e7efade9b3c5f5ba678ae0fb2223ca909ca03fe3d0a387752af20db2c2affe46a7b01',
+  },
+  mockProfileResult: {
+    name: 'baywallet',
+    username: undefined,
+    display_name: 'Bay Wallet',
+    picture: 'https://void.cat/d/JmvLC9iEZfkN8hxTJFbXRu.webp',
+    banner:
+      'https://nostr.build/i/b56a69d047968e88f2d37392c09a9870506459f4b09857d049f6f92f84d420d6.jpg',
+    about: 'tampa is the best city in the us',
+    nip05: 'baywallet@bitcoinbay.club',
+    website: 'https://github.com/bennyhodl/baywallet',
+    lud06:
+      'lnurl1dp68gurn8ghj7ampd3kx2ar0veekzar0wd5xjtnrdakj7tnhv4kxctttdehhwm30d3h82unvwqhkwmrpwdehjurjd9jhxapkxytx7p52',
+    lud16: 'glassypriest16@walletofsatoshi.com',
+    pubkey: '43c32ed61a8259ddaef6eb43c1c3114524c09690cbb92c57196b395abf73c527',
+  },
+};
--- __tests__/electrs.test.ts
+++
@@ -1,9 +0,0 @@
-import axios from "../__mock__/axios"
-// import { getTransactionData } from "../src/blocks/electrs"
-
-describe("Electrum", () => {
-  it("", () => {
-    axios.get.mockResolvedValueOnce({data: {}, status: 300})
-    expect(true).toBe(true)
-  })
-})
